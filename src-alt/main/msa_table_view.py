00eaf2a34b42c780831669b1b0fb026dc6d22125
Marc Soda <marc@soda.fm>
2024-08-18 21:30:49 -0400

    single row labels

--- src-alt/main/msa_table_view.py
+++ src-alt/main/msa_table_view.py
@@ -57,13 +57,21 @@ class MsaFiles(QAbstractTableModel):
         if not self._data:
             return None
 
-        if role == Qt.DisplayRole or role == Qt.EditRole:
+        if role == Qt.DisplayRole:
+            # Hide label for display if there's only 1 row
+            if index.column() == 0 and self.rowCount() == 1:
+                return ""
             return self._data[index.row()][index.column()]
+
+        if role == Qt.EditRole:
+            return self._data[index.row()][index.column()]
+
+        return None
 
     def setData(self, index, value, role=Qt.EditRole):
         if role == Qt.EditRole:
             # Ensure label is valid before setting
-            if index.column() == 0 and not self.is_valid_label(value, index.row()):
+            if index.column() == 0 and not self.isValidLabel(value, index.row()):
                 return False
             self._data[index.row()][index.column()] = value
             self.dataChanged.emit(index, index, [Qt.DisplayRole, Qt.EditRole])
@@ -72,16 +80,18 @@ class MsaFiles(QAbstractTableModel):
 
     def flags(self, index):
         default_flags = super().flags(index)
-        # Make the label and alignment columns editable. Disable label for a single row
-        if self.rowCount() > 1 and index.column() in [0, 2]:
-            return default_flags | Qt.ItemIsEditable
-        elif index.column() == 2:
+        # Don't allow label to be edited when only 1 row is present
+        if index.column() == 0 and self.rowCount() == 1:
+            return default_flags
+        # Make the label and alignment columns editable
+        elif index.column() in [0, 2]:
             return default_flags | Qt.ItemIsEditable
         return default_flags
 
     def headerData(self, section, orientation, role):
         if role == Qt.DisplayRole and orientation == Qt.Horizontal:
             return self._headers[section]
+        return None
 
     def insertRows(self, position, rows=1, parent=QModelIndex()):
         self.beginInsertRows(parent, position, position + rows - 1)
@@ -90,12 +100,12 @@ class MsaFiles(QAbstractTableModel):
         self.endInsertRows()
         return True
 
-    def is_valid_label(self, new_label, row):
+    def isValidLabel(self, new_label, row):
         if not new_label:
             self.error.emit("Error", "Label cannot be blank")
             return False
 
-        existing_labels = {self.data(self.index(r, 0), Qt.DisplayRole) for r in range(self.rowCount()) if r != row}
+        existing_labels = {self.data(self.index(r, 0), Qt.EditRole) for r in range(self.rowCount()) if r != row}
         if new_label in existing_labels:
             self.error.emit('Error', "Labels must be unique")
             return False
@@ -166,7 +176,6 @@ class MsaTableView(QWidget):
         self.table.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
         self.table.setColumnWidth(2, self.table.fontMetrics().horizontalAdvance(self.model._headers[2]) + 50)
         self.table.setColumnWidth(3, self.table.fontMetrics().horizontalAdvance("Sequences: XXXXX") + 50)
-
         self.table.setColumnWidth(4, 16 + 10)
 
         # Remove the dummy row used to calculate table height
@@ -189,22 +198,22 @@ class MsaTableView(QWidget):
     def add_files(self):
         files = QFileDialog.getOpenFileNames()[0]
         label_gen = MsaTableView.label_gen()
-        existing_labels = {self.model.data(self.model.index(row, 0), Qt.DisplayRole) for row in range(self.model.rowCount())}
-        existing_files = {self.model.data(self.model.index(row, 1), Qt.DisplayRole) for row in range(self.model.rowCount())}
+        existing_labels = {self.model.data(self.model.index(row, 0), Qt.EditRole) for row in range(self.model.rowCount())}
+        existing_files = {self.model.data(self.model.index(row, 1), Qt.EditRole) for row in range(self.model.rowCount())}
 
         for file in files:
             # Just skip files that already exist
             if file not in existing_files:
-                # Make sure labels are unique
                 label = next(label_gen)
+                # Make sure labels are unique
                 while label in existing_labels:
                     label = next(label_gen)
 
                 self.model.insertRows(self.model.rowCount(), 1)
-                self.model.setData(self.model.index(self.model.rowCount() - 1, 0), label, Qt.EditRole)
-                self.model.setData(self.model.index(self.model.rowCount() - 1, 1), file, Qt.EditRole)
-                self.set_remove_button(self.model.rowCount() - 1)
-
+                row = self.model.rowCount() - 1
+                self.model.setData(self.model.index(row, 0), label, Qt.EditRole)
+                self.model.setData(self.model.index(row, 1), file, Qt.EditRole)
+                self.set_remove_button(row)
                 existing_files.add(file)
                 existing_labels.add(label)
 
